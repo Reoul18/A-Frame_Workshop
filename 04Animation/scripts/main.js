ee0893583b434ef53fd6ab0d135acb2aab096baf
Reoul18 <rebeccaleonora18@gmail.com>
2021-05-18 22:42:12 +0200

    Adding 2 speeds
    First Try

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,13 +1,21 @@
 
 
 // changing individual properties with code and using setInterval
-var rotationSpeed = -0.01;
+// var rotationSpeed = -0.01;
+var rotationSpeedFast = 0.01;
+var rotationSpeedSlow = -0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
-	myOtherBox.object3D.rotation.y += rotationSpeed;
-	myOtherBox.object3D.rotation.z += rotationSpeed;
+	myOtherBox.object3D.rotation.x += rotationSpeedFast;
+	myOtherBox.object3D.rotation.y += rotationSpeedFast;
+	myOtherBox.object3D.rotation.z += rotationSpeedFast;
+	console.log(myOtherBox.object3D.rotation.x);
+	console.log(myOtherBox.object3D.rotation.y);
+	console.log(myOtherBox.object3D.rotation.z);
+	myOtherBox.object3D.rotation.x += rotationSpeedSlow;
+	myOtherBox.object3D.rotation.y += rotationSpeedSlow;
+	myOtherBox.object3D.rotation.z += rotationSpeedSlow;
 	console.log(myOtherBox.object3D.rotation.x);
 	console.log(myOtherBox.object3D.rotation.y);
 	console.log(myOtherBox.object3D.rotation.z);
