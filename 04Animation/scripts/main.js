7cf53cbfcd0a4c0cb381329deec3cdc000e91aa8
Reoul18 <rebeccaleonora18@gmail.com>
2021-06-24 04:24:00 +0200

    BACK

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,29 +1,18 @@
 
 
 // changing individual properties with code and using setInterval
-var rotationSpeed = 0.01;
+// var rotationSpeed = -0.01;
+var rotationSpeedFast = 0.05;
+var rotationSpeedSlow = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
+	myOtherBox.object3D.rotation.x += rotationSpeedSlow;
+	myOtherBox.object3D.rotation.y += rotationSpeedSlow;
+	myOtherBox.object3D.rotation.z += rotationSpeedFast;
 	console.log(myOtherBox.object3D.rotation.x);
+	console.log(myOtherBox.object3D.rotation.y);
+	console.log(myOtherBox.object3D.rotation.z);
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
-
-// // changing individual properties with code and using setInterval
-// // var rotationSpeed = -0.01;
-// var rotationSpeedFast = 0.05;
-// var rotationSpeedSlow = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
-
-// function spin(){
-// 	myOtherBox.object3D.rotation.x += rotationSpeedSlow;
-// 	myOtherBox.object3D.rotation.y += rotationSpeedSlow;
-// 	myOtherBox.object3D.rotation.z += rotationSpeedFast;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// 	console.log(myOtherBox.object3D.rotation.y);
-// 	console.log(myOtherBox.object3D.rotation.z);
-// }
-
-// setInterval(spin, 16); //equivalent to 60 fps
